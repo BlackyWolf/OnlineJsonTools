c058be9b7f606dda40457eb06d74e3fb44666757
Matthew 𝔸𝕝𝕕𝕒𝕔𝕠 <Matt.Aldaco@csiweb.com>
2023-04-17 18:04:44 -0500

    initial commit

---
+++ src/src/App.tsx
@@ -0,0 +1,7 @@
+import type { Component } from 'solid-js';
+
+export const App: Component = () => {
+    return (
+        <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    );
+};
